dfc69b71219b6d3a8515d8ef31b93012f8fb96fe
Virginiiamz <virginia.munoz-castro@iesruizgijon.com>
2024-10-16 21:41:41 +0200

    Creado una alerta en la pagina de Inicio, cada vez que se pulse el boton de "iniciar sesion"

---
+++ src/js/alertaInicio.js
@@ -0,0 +1,19 @@
+const alertPlaceholder = document.getElementById('alertaInicSesion')
+const appendAlert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+
+    alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('iniciarSesion')
+if (alertTrigger) {
+    alertTrigger.addEventListener('click', () => {
+        appendAlert('Por ahora no puedes iniciar sesion. Vuelve a intentarlo proximamente...', 'danger')
+    })
+}
