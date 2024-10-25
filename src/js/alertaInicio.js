4b142a00c2ff7ac8e5a992c72032146a2fc5ac7a
Virginiiamz <virginia.munoz-castro@iesruizgijon.com>
2024-10-25 17:11:49 +0200

    Cambios en la alerta de iniciar sesion

--- src/js/alertaInicio.js
+++ src/js/alertaInicio.js
@@ -2,7 +2,7 @@ const alertPlaceholder = document.getElementById('alertaInicSesion')
 const appendAlert = (message, type) => {
     const wrapper = document.createElement('div')
     wrapper.innerHTML = [
-        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `<div id="alertaInicio" class="alert alert-${type} alert-dismissible" role="alert">`,
         `   <div>${message}</div>`,
         '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
         '</div>'
