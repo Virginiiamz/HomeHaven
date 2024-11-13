9cd99e3dc795af893db16bbb3cc5f611f1b03608
Virginiiamz <virginia.munoz-castro@iesruizgijon.com>
2024-11-13 10:13:31 +0100

    Creado funciones de mostrar y ocultar los formularios

--- src/js/codigo.js
+++ src/js/codigo.js
@@ -5,13 +5,36 @@ var oInmobiliaria = new Inmobiliaria();
 registrarEventos();
 
 function registrarEventos() {
-    
+
+    document.querySelector("#mnuAltaPropiedad").addEventListener("click", mostrarFormulario);
+
     frmAltaPropiedad.AltaPropiedadBoton.addEventListener("click", procesarAltaPropiedad);
 
 }
 
+function mostrarFormulario(oEvento) {
+
+    let opcion = oEvento.target.id;
+
+    ocultarFormulario();
+
+    switch (opcion) {
+        case "mnuAltaPropiedad":
+            frmAltaPropiedad.style.display = "block";
+            break;
+
+        default:
+            break;
+    }
+
+}
+
+function ocultarFormulario() {
+    frmAltaPropiedad.style.display = "none";
+}
+
 async function procesarAltaPropiedad() {
-    
+
     let direccion = frmAltaPropiedad.AltaPropiedadDireccion.value.trim();
     let precio = parseFloat(frmAltaPropiedad.AltaPropiedadPrecio.value);
     let tipoPropiedad = frmAltaPropiedad.AltaPropiedadTipo.value;
